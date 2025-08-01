820abe33748b9d68b00c4c80680eb996efa288c3
zasimmallick <zasimmallickofficial@gmail.com>
2025-08-01 06:24:49 +0600

    ES6 modules add

--- es6-module-overview/src/App.jsx
+++ es6-module-overview/src/App.jsx
@@ -1,33 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import Watch from './components/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [watches, setWatches] = useState([]);
+
+  // useEffect( () =>{
+  //   fetch('watches.json')
+  //   .then(res=> res.json())
+  //   .then(data =>setWatches(data));
+  // }, [])
+  useEffect( () =>{
+    fetch('https://raw.githubusercontent.com/jhankarpHero/watches-data/main/watches.json')
+    .then(res=> res.json())
+    .then(data =>setWatches(data));
+  }, [])
+  
+  // const watches = [
+  //   {id: 1, name: 'Apple watch', price: 200},
+  //   {id: 2, name: 'Samsu watch', price: 200},
+  //   {id: 3, name: 'MI watch', price: 200},
+  // ]
+
+  // const watches = [
+  //   {
+  //     id: 1,
+  //     name: 'Apple Watch Series 7',
+  //     price: 399,
+  //     // ...other properties
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Galaxy Watch 4',
+  //     price: 249,
+  //     // ...other properties
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Fitbit Versa 3',
+  //     price: 229,
+  //     // ...other properties
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Garmin Venu 2',
+  //     price: 399,
+  //     // ...other properties
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Huawei Watch GT 3',
+  //     price: 299,
+  //     // ...other properties
+  //   },
+  // ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
     </>
   )
 }
